78c12ee2a2b0adae2b87fee87aa1d7728ed4ba21
Alessandro Buonpane <Alessandro Buonpane>
2016-07-22 17:08:10 +0200

    rope added

--- Assets/Scripts/Triangle/Hook.cs
+++ Assets/Scripts/Triangle/Hook.cs
@@ -6,14 +6,27 @@ public class Hook : MonoBehaviour {
     public GameObject player;
     public bool hooked = false;
     Vector2 dir;
-    public int speed; 
+    public int speed;
+    LineRenderer lr; 
 	
+
+    void Start()
+    {
+        lr = GetComponentInChildren<LineRenderer>();
+        lr.SetPosition(0, player.transform.position); 
+    }
+
 	// Update is called once per frame
 	void Update () {
+        lr.SetPosition(0, player.transform.position);
+        lr.SetPosition(1, transform.position);
         if (hooked)
         {
-            player.GetComponent<Rigidbody2D>().AddForce(dir.normalized * speed);  
+            player.GetComponent<Rigidbody2D>().AddForce(dir.normalized * speed);
         }
+       
+           
+        
 	}
 
     void OnTriggerEnter2D(Collider2D coll)
