cefe5fedcdcc7feff53d4e0922e2d6018768ca2d
Alessandro Buonpane <Alessandro Buonpane>
2016-07-22 14:11:50 +0200

    circle shoots!

---
+++ Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed;
+    public float ttl = 3;
+    float timeLived = 0;
+    public Vector2 dir; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLived += Time.deltaTime;
+        if (timeLived >= ttl)
+            Destroy(gameObject);
+        transform.position += (Vector3)dir.normalized * Time.deltaTime * speed; 
+	}
+}
---
+++ Assets/Gun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject gun;
+    public float shootFrequency = 0.5f;
+    float counter; 
+
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime; 
+        if (Input.GetMouseButtonDown(0) && counter>=shootFrequency)
+        {
+            counter = 0; 
+            GameObject bullet = (GameObject)Instantiate(Resources.Load("Bullet"), gun.transform.position, Quaternion.identity);
+            bullet.GetComponent<Bullet>().dir = GameObject.Find("Crosshair").transform.position - transform.position;
+        }
+	}
+}
