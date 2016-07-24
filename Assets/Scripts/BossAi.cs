638af29265551f2729bd859327e0cc9995186f85
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2016-07-24 20:17:06 +0200

    volume enraged ridotto

--- Assets/Scripts/BossAi.cs
+++ Assets/Scripts/BossAi.cs
@@ -16,20 +16,6 @@ public class BossAi : MonoBehaviour {
     public AudioClip[] hitClips;
     public GameObject gameController;
 
-	 // Use this for initialization
-	void Start () {
-        //StartCoroutine(AIMovementLoop());
-        //attivo in modo scriptato la classe "Square"
-        //transform.GetChild(1).gameObject.SetActive(false);
-        //current = transform.GetChild(0).gameObject;
-        //currentClass = Classes.Square;
-        //healthBar.MaxValue = current.GetComponent<IBossClass>().getHP();
-        //audio = GetComponent<AudioSource>();
-        //if (gameController == null)
-        //    gameController = GameObject.Find("GameController");
-        //print("CAZZO");
-    }
-
     void Init(int child)
     {
         transform.GetChild(0).gameObject.SetActive(false);
@@ -127,39 +113,6 @@ public class BossAi : MonoBehaviour {
                     currentClass = Classes.Square;
                 }
                 break;
-        }
-    }
-
-
-    IEnumerator AIMovementLoop()
-    {
-        while (true)//finchè non muoio
-        {
-            if (currentState != States.Frozen)
-            {
-                print("Aspetto");
-                //aspetto un tempo random di secondi
-                yield return new WaitForSeconds(Random.Range(0.5f, 2f));
-                print("fine attesa");
-                //decido la direzione e la quantità di spostamento a random
-                float delta = Random.Range(-2f, 2f) * 15;
-                foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
-                {
-                    sr.flipX = delta > 0 ? true : false;
-                }
-                //TODO: controllare che il boss non stia uscendo dalla sua zona
-                //mi sposto
-                Vector2 startPosition = transform.position;
-                while (Vector2.Distance(transform.position, startPosition + new Vector2(delta, 0)) > 1f)
-                {
-                    transform.position = Vector2.Lerp(transform.position, startPosition + new Vector2(delta, 0), Time.deltaTime * 2);
-                    yield return new WaitForEndOfFrame();
-                }
-                print("Fine spostamento");
-            }else
-            {
-                yield return new WaitForSeconds(4f);
-            }
         }
     }
 
