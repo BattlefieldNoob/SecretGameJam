10b4965be6f39f056303061691e850e08371ce98
Alessandro Buonpane <Alessandro Buonpane>
2016-07-24 17:52:13 +0200

    game ends

--- Assets/Scripts/SquareBossClass.cs
+++ Assets/Scripts/SquareBossClass.cs
@@ -147,6 +147,7 @@ public class SquareBossClass : MonoBehaviour, IBossClass
             {
                 stopped = true;
                 GetComponentInParent<BossAi>().SendMessage("CubeDeath");
+                GameObject.Find("GameController").SendMessage("TheEnd"); 
             }
         }
     }
---
+++ Assets/TheEndText.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TheEndText : MonoBehaviour {
+
+    public RawImage image;
+    bool doing; 
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+    void DoIt()
+    {
+        doing = true; 
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (doing)
+        {
+            Color c = image.color;
+            c.a += 0.01f;
+            image.color = c;
+            if (c.a >= 1)
+                StartCoroutine(BackToMainScreen());
+        }
+
+        
+	}
+
+    IEnumerator BackToMainScreen()
+    {
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene(0); 
+    }
+}
